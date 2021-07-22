9019a06c2b03c0509469fc9e571da362db5157c9
amrsekilly <github@amrsekilly.com>
2021-07-22 17:35:41 +0000

    Deploying to master from  @ f33a0728dc2a646f1bdf72fed1dba75751a106be 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6eda62f0ed892d1be09d.js"
+    "url": "webpack-runtime-773107142038ca1d3e5c.js"
   },
   {
     "url": "framework-e3ca44aab963e64b4c36.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "80232822d1df51fb5e2de6f4d0d7eab6"
+    "revision": "4bd94ea560cfcf1bcf66ed5a7f72497b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
